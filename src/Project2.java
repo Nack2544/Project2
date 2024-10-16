c606c745ce83817b2bcd876c275cb79f02305bcd
Nack2544 <nacksuphakrit@gmail.com>
2024-10-15 20:48:03 -0400

    add ArrayList and complete adding all java classes

---
+++ src/Project2.java
@@ -0,0 +1,20 @@
+import java.io.File;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Project2 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        FileHandler fileHandler = new FileHandler();
+        ArrayList<RoadVolume> volumeData = new ArrayList<>();
+        ArrayList<RoadSpeed> speedData = new ArrayList<>();
+        ArrayList<RoadSection> roadSections = new ArrayList<>();
+        boolean found = false;
+
+
+
+    }
+}
