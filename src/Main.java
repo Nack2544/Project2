8359e45b2456e002688d89955294cc3561b30bab
Nack2544 <nacksuphakrit@gmail.com>
2024-10-15 20:21:28 -0400

    testing modify

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        String input = in.next();
+        System.out.println("provide the file name: ");
+        File file = new File(input);
     }
 }
